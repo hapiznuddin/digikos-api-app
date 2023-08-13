cf6ec9c32400b9aeb47b7ecfa4e1f8505efec203
Hapiz Nuddin <hapiznuddin@gmail.com>
2023-08-13 08:46:11 +0700

    feat(api): login & register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,41 +8,43 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login(Request $request) {
-        $request->validate([
-            'email' => 'required',
-            'password' => 'required',
-        ]);
+  public function login(Request $request)
+  {
+    $request->validate([
+      'email' => 'required',
+      'password' => 'required',
+    ]);
 
-        $user = User::where('email', $request->email)->first();
-        if (! $user || ! Hash::check($request->password, $user->password) || ! $user->active) {
-            return response()->json([
-                'message' => 'credentials does not match',
-            ], 401);
-        }
-
-        $token = $user->createToken('access_token')->plainTextToken;
-        return response()->json([
-            'token' => $token,
-        ]);
-
+    $user = User::where('email', $request->email)->first();
+    if (!$user || !Hash::check($request->password, $user->password) || !$user->active) {
+      return response()->json([
+        'message' => 'credentials does not match',
+      ], 401);
     }
 
-    public function register(Request $request) {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required|string',
-            'password' => 'required|string|min:8',
-        ]);
+    $token = $user->createToken('access_token')->plainTextToken;
+    return response()->json([
+      'token' => $token,
+      'role' => $user->role->name,
+    ]);
+  }
 
-        User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+  public function register(Request $request)
+  {
+    $request->validate([
+      'name' => 'required',
+      'email' => 'required|string',
+      'password' => 'required|string|min:8',
+    ]);
 
-        return response()->json([
-            'message' => 'Berhasil mendaftar',
-        ], 201);
-    }
+    User::create([
+      'name' => $request->name,
+      'email' => $request->email,
+      'password' => Hash::make($request->password),
+    ]);
+
+    return response()->json([
+      'message' => 'Berhasil mendaftar',
+    ], 201);
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,6 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group( function (Request $request) {
+// Route::middleware('auth:sanctum')->group( function (Request $request) {
     
-});
+// });
