2b82a32653442f5161ff77372af27f17ec49e50c
Hapiz Nuddin Setiadi <hapiznuddin@gmail.com>
2023-11-20 23:20:37 +0700

    fix(report): tambahan data tipe expense

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -19,27 +19,43 @@ class ReportController extends Controller
         $incomeQuery = Payment::query();
         $expenseQuery = Expense::query();
 
-    if ($request->has('month') && $request->month !== null) {
-        $incomeQuery->whereYear('created_at', $request->year)->whereMonth('created_at', $request->month);
-        $expenseQuery->whereYear('date_paid', $request->year)->whereMonth('date_paid', $request->month);
-    } else {
-        $incomeQuery->whereYear('created_at', $request->year);
-        $expenseQuery->whereYear('date_paid', $request->year);
-    }
+        if ($request->has('month') && $request->month !== null) {
+            $incomeQuery->whereYear('created_at', $request->year)->whereMonth('created_at', $request->month);
+            $expenseQuery->whereYear('date_paid', $request->year)->whereMonth('date_paid', $request->month);
+        } else {
+            $incomeQuery->whereYear('created_at', $request->year);
+            $expenseQuery->whereYear('date_paid', $request->year);
+        }
 
-    $reportIncome = $incomeQuery
-        ->selectRaw('COUNT(*) as total_payment, SUM(price) as total_price, MONTHNAME(created_at) as month_name, YEAR(created_at) as year_name')
-        ->groupByRaw('YEAR(created_at), MONTHNAME(created_at)')
+
+        $reportIncome = $incomeQuery
+            ->selectRaw('COUNT(*) as total_payment, SUM(price) as total_price, MONTHNAME(created_at) as month_name, YEAR(created_at) as year')
+            ->groupByRaw('YEAR(created_at), MONTHNAME(created_at)')
+            ->get(); 
+
+        $reportExpense = $expenseQuery
+            ->selectRaw('COUNT(*) as total_expense, SUM(total_payment) as total_price, MONTHNAME(date_paid) as month_name, YEAR(date_paid) as year')
+            ->groupByRaw('YEAR(date_paid), MONTHNAME(date_paid)')
+            ->get(); 
+
+        $expenseCategory = Expense::whereYear('date_paid', $request->year)->whereMonth('date_paid', $request->month);
+
+        if ($request->has('month') && $request->month !== null) {
+            $expenseCategory->whereYear('date_paid', $request->year)->whereMonth('date_paid', $request->month);
+        } else {
+            $expenseCategory->whereYear('date_paid', $request->year);
+        }
+
+        $reportExpenseType = $expenseCategory
+        ->select('expense', 'total_payment')
         ->get();
 
-    $reportExpense = $expenseQuery
-        ->selectRaw('COUNT(*) as total_expense, SUM(total_payment) as total_price, MONTHNAME(date_paid) as month_name, YEAR(date_paid) as year_name')
-        ->groupByRaw('YEAR(date_paid), MONTHNAME(date_paid)')
-        ->get();
-
-        return response()->json([
+        return response()->json(
+        [
             'income' => $reportIncome,
             'expense' => $reportExpense,
-        ], 200);
+            'expense_category' => $reportExpenseType
+        ]
+        , 200);
     }
 }
