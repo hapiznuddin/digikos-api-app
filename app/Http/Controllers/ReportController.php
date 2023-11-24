cce8f8f0549840cefd014828e1ac41a59022bcd9
Hapiz Nuddin Setiadi <hapiznuddin@gmail.com>
2023-11-25 00:20:22 +0700

    install pdf

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Expense;
 use App\Models\Payment;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
@@ -27,16 +28,15 @@ class ReportController extends Controller
             $expenseQuery->whereYear('date_paid', $request->year);
         }
 
-
         $reportIncome = $incomeQuery
             ->selectRaw('COUNT(*) as total_payment, SUM(price) as total_price, MONTHNAME(created_at) as month_name, YEAR(created_at) as year')
             ->groupByRaw('YEAR(created_at), MONTHNAME(created_at)')
-            ->get(); 
+            ->get();
 
         $reportExpense = $expenseQuery
             ->selectRaw('COUNT(*) as total_expense, SUM(total_payment) as total_price, MONTHNAME(date_paid) as month_name, YEAR(date_paid) as year')
             ->groupByRaw('YEAR(date_paid), MONTHNAME(date_paid)')
-            ->get(); 
+            ->get();
 
         $expenseCategory = Expense::whereYear('date_paid', $request->year)->whereMonth('date_paid', $request->month);
 
@@ -47,15 +47,16 @@ class ReportController extends Controller
         }
 
         $reportExpenseType = $expenseCategory
-        ->select('expense', 'total_payment')
-        ->get();
+            ->select('expense', 'total_payment')
+            ->get();
 
         return response()->json(
-        [
-            'income' => $reportIncome,
-            'expense' => $reportExpense,
-            'expense_category' => $reportExpenseType
-        ]
-        , 200);
+            [
+                'income' => $reportIncome,
+                'expense' => $reportExpense,
+                'expense_category' => $reportExpenseType,
+            ],
+            200
+        );
     }
 }
